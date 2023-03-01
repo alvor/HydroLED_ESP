4f633eec5c6de56a1523d37b26bd2a4f1412f422
alvor <alvor1975@gmail.com>
2023-03-01 20:59:23 +0700

    Это уже не влазит в память 8266. Будет продолжатся как ветвь для других контроллеров

--- ds2406.py
+++ ds2406.py
@@ -1,8 +1,8 @@
-def turn(self, Rom, PinA=0, PinB=0):
-    self.ow.reset()
-    self.ow.select_rom(Rom)
+def turn(ow, Rom, PinA=0, PinB=0):
+    ow.reset()
+    ow.select_rom(Rom)
     _sb = PinA * 64 + PinB * 32
-    self.ow.write([85, 7, 0, _sb])
+    ow.write([85, 7, 0, _sb])
     for _n in range(1, 6):
-        self.ow.readbyte()
+        ow.readbyte()
 
--- hl.py
+++ hl.py
@@ -9,26 +9,31 @@ class hl:
     def off(self):
         pass
 
-class hl_ow(hl):
-    def __init__(self, name,pin,adr):
-        super().__init__(name)
-        self.pin,self.adr = pin,adr
+class _ow():
+    def __init__(self,ow,adr):
+        self.ow,self.adr = ow,adr
 
 
-class hl_ow_2406(hl_ow):
-    def __init__(self, name,pin,adr):
-        super().__init__(name,pin,adr)
+class _ow_2406(_ow):
+    def __init__(self, ow,adr):
+        super().__init__(ow,adr)
         self.ch1,self.ch2=0,0
     def ch_set(self,n,val):
         if n!=0: self.ch1=val
         if n!=1: self.ch2=val
         ds2406.turn(self.pin,h2b(self.adr),self.ch1,self.ch2)
 
-class hl_ow_2406_ch():
-    def __init__(self,p2406,n):
+class _ow_18x20(_ow):
+    def __init__(self, ow,adr):
+        super().__init__(ow, adr)
+
+class hl_2406_ch(hl):
+    def __init__(self, name, p2406,n):
+        super.__init__(self,name)
         self.p2406,self.n=p2406,n
-
-class hl_ow_18x20(hl_ow):
-    def __init__(self, name,pin,adr):
-        super().__init__(name, pin, adr)
-
+    def turn(self,val):
+        self.p2406.ch_set(self.n,val)
+    def on(self):
+        self.turn(self,0)
+    def off(self):
+        self.turn(self,1)
